83f0c428c66af5ffb513da001b34cae1a38e2ecc
hoangdv <hoang.dv@outlook.com>
2020-07-26 11:30:08 +0700

    Add configure minimum threshold enforcement for coverage results

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,14 @@ module.exports = {
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   coverageReporters: ["html", "text", "lcov"],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  },
   testPathIgnorePatterns: [
     "/dist/",
     "/node_modules/"
