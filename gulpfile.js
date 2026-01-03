1784a0991b66b8ebc1fef3d6b575cb17c6ee48cb
Hoang Dinh <hoang.dv@outlook.com>
2026-01-03 10:52:24 +0900

    chore: remove unused gulpfile.js and jest.config.js

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-// const { src, dest } = require('gulp');
-//
-// function copyIcons() {
-//   return src('src/assets/**/*.png')
-//     .pipe(dest('dist/assets'));
-// }
-//
-// exports.default = copyIcons;
